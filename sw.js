6aa6a2fbaf1e3bfa3bb424dfa527f7dab383f436
ndawulaaxam-netizen <ndawulaaxam@gmail.com>
2026-08-23 01:25:37 +0100

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,52 +1,113 @@
-var CACHE = 'dukaflow-v9-force';
-var SHELL = ['./', './index.html', './logo.png'];
+var CACHE = 'dukaflow-v12-smart-inject';
+var SHELL = [
+  './',
+  './index.html',
+  './manifest.json',
+  './smartsync.js',
+  './icon-512.png',
+  './logo.png'
+];
 
 function clearAllCaches() {
-  return caches.keys().then(function(keys) {
-    return Promise.all(keys.map(function(k) {
+  return caches.keys().then(function (keys) {
+    return Promise.all(keys.map(function (k) {
       return caches.delete(k);
     }));
   });
 }
 
 function forceUpdate() {
-  return clearAllCaches().then(function() {
-    return caches.open(CACHE).then(function(c) {
+  return clearAllCaches().then(function () {
+    return caches.open(CACHE).then(function (c) {
       return c.addAll(SHELL);
     });
   });
 }
 
-self.addEventListener('install', function(e) {
+function pingClients() {
+  return self.clients.matchAll({
+    type: 'window',
+    includeUncontrolled: true
+  }).then(function (list) {
+    list.forEach(function (client) {
+      client.postMessage({ type: 'DF_SYNC_NOW' });
+    });
+  });
+}
+
+function injectSmartSync(response) {
+  return response.text().then(function (html) {
+    if (html.indexOf('smartsync.js') === -1) {
+      html = html.replace('</body>', '<script src="smartsync.js"></script></body>');
+    }
+
+    return new Response(html, {
+      status: response.status,
+      statusText: response.statusText,
+      headers: {
+        'Content-Type': 'text/html; charset=utf-8',
+        'Cache-Control': 'no-store'
+      }
+    });
+  });
+}
+
+function fetchIndexWithInjection(request) {
+  return fetch(request, { cache: 'no-store' }).then(function (response) {
+    return injectSmartSync(response);
+  }).catch(function () {
+    return caches.match('./index.html').then(function (cached) {
+      if (cached) return injectSmartSync(cached);
+      return caches.match('./');
+    });
+  });
+}
+
+self.addEventListener('install', function (e) {
   e.waitUntil(
-    caches.open(CACHE).then(function(c) {
+    caches.open(CACHE).then(function (c) {
       return c.addAll(SHELL);
     })
   );
   self.skipWaiting();
 });
 
-self.addEventListener('activate', function(e) {
+self.addEventListener('activate', function (e) {
   e.waitUntil(
-    caches.keys().then(function(keys) {
-      return Promise.all(keys.map(function(k) {
-        if (k !== CACHE) return caches.delete(k);
+    caches.keys().then(function (keys) {
+      return Promise.all(keys.map(function (k) {
+        if (k !== CACHE) {
+          return caches.delete(k);
+        }
       }));
-    }).then(function() {
+    }).then(function () {
       return self.clients.claim();
     })
   );
 });
 
-self.addEventListener('message', function(e) {
+self.addEventListener('message', function (e) {
   if (e.data && e.data.type === 'DF_FORCE_UPDATE') {
     e.waitUntil(forceUpdate());
   }
 });
 
-self.addEventListener('fetch', function(e) {
+self.addEventListener('sync', function (e) {
+  if (e.tag === 'dukaflow-sync') {
+    e.waitUntil(pingClients());
+  }
+});
+
+self.addEventListener('periodicsync', function (e) {
+  if (e.tag === 'dukaflow-periodic') {
+    e.waitUntil(pingClients());
+  }
+});
+
+self.addEventListener('fetch', function (e) {
   if (e.request.mode === 'navigate') {
     var force = false;
+
     try {
       var u = new URL(e.request.url);
       force = u.search.indexOf('dfupdate=1') > -1;
@@ -56,28 +117,27 @@ self.addEventListener('fetch', function(e) {
 
     if (force) {
       e.respondWith(
-        fetch(e.request, { cache: 'no-store' }).then(function(response) {
-          return forceUpdate().then(function() {
-            return response;
+        fetch(e.request, { cache: 'no-store' }).then(function (response) {
+          return forceUpdate().then(function () {
+            return injectSmartSync(response);
           });
-        }).catch(function() {
-          return caches.match('./index.html');
+        }).catch(function () {
+          return caches.match('./index.html').then(function (cached) {
+            if (cached) return injectSmartSync(cached);
+            return caches.match('./');
+          });
         })
       );
       return;
     }
 
-    e.respondWith(
-      fetch(e.request, { cache: 'no-store' }).catch(function() {
-        return caches.match('./index.html');
-      })
-    );
+    e.respondWith(fetchIndexWithInjection(e.request));
     return;
   }
 
   e.respondWith(
-    caches.match(e.request).then(function(r) {
-      return r || fetch(e.request);
+    caches.match(e.request).then(function (cached) {
+      return cached || fetch(e.request);
     })
   );
 });
