0e6edb6d54c30d86360ade7866c096e62311d510
ndawulaaxam-netizen <ndawulaaxam@gmail.com>
2026-09-07 14:03:24 +0300

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,143 +1,24 @@
-var CACHE = 'dukaflow-v12-smart-inject';
-var SHELL = [
-  './',
-  './index.html',
-  './manifest.json',
-  './smartsync.js',
-  './icon-512.png',
-  './logo.png'
-];
-
-function clearAllCaches() {
-  return caches.keys().then(function (keys) {
-    return Promise.all(keys.map(function (k) {
-      return caches.delete(k);
-    }));
-  });
-}
-
-function forceUpdate() {
-  return clearAllCaches().then(function () {
-    return caches.open(CACHE).then(function (c) {
-      return c.addAll(SHELL);
-    });
-  });
-}
-
-function pingClients() {
-  return self.clients.matchAll({
-    type: 'window',
-    includeUncontrolled: true
-  }).then(function (list) {
-    list.forEach(function (client) {
-      client.postMessage({ type: 'DF_SYNC_NOW' });
-    });
-  });
-}
-
-function injectSmartSync(response) {
-  return response.text().then(function (html) {
-    if (html.indexOf('smartsync.js') === -1) {
-      html = html.replace('</body>', '<script src="smartsync.js"></script></body>');
-    }
-
-    return new Response(html, {
-      status: response.status,
-      statusText: response.statusText,
-      headers: {
-        'Content-Type': 'text/html; charset=utf-8',
-        'Cache-Control': 'no-store'
-      }
-    });
-  });
-}
-
-function fetchIndexWithInjection(request) {
-  return fetch(request, { cache: 'no-store' }).then(function (response) {
-    return injectSmartSync(response);
-  }).catch(function () {
-    return caches.match('./index.html').then(function (cached) {
-      if (cached) return injectSmartSync(cached);
-      return caches.match('./');
-    });
-  });
-}
-
-self.addEventListener('install', function (e) {
-  e.waitUntil(
-    caches.open(CACHE).then(function (c) {
-      return c.addAll(SHELL);
-    })
-  );
-  self.skipWaiting();
+var CACHE='dukaflow-v75';
+self.addEventListener('install',function(e){self.skipWaiting()});
+self.addEventListener('activate',function(e){
+  e.waitUntil(caches.keys().then(function(ks){return Promise.all(ks.filter(function(k){return k!==CACHE}).map(function(k){return caches.delete(k)}))}).then(function(){return self.clients.claim()}));
 });
-
-self.addEventListener('activate', function (e) {
-  e.waitUntil(
-    caches.keys().then(function (keys) {
-      return Promise.all(keys.map(function (k) {
-        if (k !== CACHE) {
-          return caches.delete(k);
-        }
-      }));
-    }).then(function () {
-      return self.clients.claim();
-    })
-  );
-});
-
-self.addEventListener('message', function (e) {
-  if (e.data && e.data.type === 'DF_FORCE_UPDATE') {
-    e.waitUntil(forceUpdate());
+self.addEventListener('fetch',function(e){
+  if(e.request.mode==='navigate'){
+    e.respondWith(
+      fetch(e.request).then(function(res){
+        var c=res.clone();caches.open(CACHE).then(function(ca){ca.put(e.request,c)});return res;
+      }).catch(function(){
+        return caches.match(e.request).then(function(m){return m||caches.match('./index.html')});
+      })
+    );
+  } else {
+    e.respondWith(
+      caches.match(e.request).then(function(m){
+        return m||fetch(e.request).then(function(res){
+          var c=res.clone();caches.open(CACHE).then(function(ca){ca.put(e.request,c)});return res;
+        });
+      })
+    );
   }
-});
-
-self.addEventListener('sync', function (e) {
-  if (e.tag === 'dukaflow-sync') {
-    e.waitUntil(pingClients());
-  }
-});
-
-self.addEventListener('periodicsync', function (e) {
-  if (e.tag === 'dukaflow-periodic') {
-    e.waitUntil(pingClients());
-  }
-});
-
-self.addEventListener('fetch', function (e) {
-  if (e.request.mode === 'navigate') {
-    var force = false;
-
-    try {
-      var u = new URL(e.request.url);
-      force = u.search.indexOf('dfupdate=1') > -1;
-    } catch (err) {
-      force = false;
-    }
-
-    if (force) {
-      e.respondWith(
-        fetch(e.request, { cache: 'no-store' }).then(function (response) {
-          return forceUpdate().then(function () {
-            return injectSmartSync(response);
-          });
-        }).catch(function () {
-          return caches.match('./index.html').then(function (cached) {
-            if (cached) return injectSmartSync(cached);
-            return caches.match('./');
-          });
-        })
-      );
-      return;
-    }
-
-    e.respondWith(fetchIndexWithInjection(e.request));
-    return;
-  }
-
-  e.respondWith(
-    caches.match(e.request).then(function (cached) {
-      return cached || fetch(e.request);
-    })
-  );
 });
